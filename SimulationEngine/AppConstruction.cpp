61df2ae679e3634c886f77530790a484fb67a979
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2025-09-02 14:57:18 -0400

    Created the general layout for the main application and created the main function logic.

---
+++ SimulationEngine/AppConstruction.cpp
@@ -0,0 +1,27 @@
+#include "Application.h"
+
+Application::Application(void)
+{
+
+}
+
+Application::~Application(void)
+{
+
+}
+
+Application& Application::operator =(const Application& a_pApp)
+{
+	Application app = Application();
+	return app;
+}
+
+Application::Application(const Application& a_pApp)
+{
+
+}
+
+void Application::Run(void)
+{
+
+}
---
+++ SimulationEngine/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Application.h"
+
+int main(void)
+{
+	// Cosntructing the Application.
+	Application* app = new Application();
+
+	// Running the Application.
+	app->Run();
+
+	// Reallocating the memory.
+	SafeDelete(app);
+
+	// Ensuring that there are no memory leaks.
+	if (_CrtDumpMemoryLeaks())
+	{
+		std::cout << "ERROR: Memory leaks present." << std::endl;
+	}
+
+	return 0;
+}
