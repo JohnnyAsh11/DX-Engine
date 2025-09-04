00c468fb3c2c53383f7af29857e15e4d2dbcd353
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2025-09-04 14:35:08 -0400

    Adjusted the Simulation code for testing with the Mesh class.  Not fully functional, needs Shader creation.

--- SimulationEngine/Simulation.cpp
+++ SimulationEngine/Simulation.cpp
@@ -1,8 +1,29 @@
 #include "Simulation.h"
 #include "Graphics.h"
+#include <vector>
+
+typedef DirectX::XMFLOAT3 Vector3;
 
 void Simulation::Init()
 {
+	Vertex* vertices = new Vertex[3];
+	unsigned int* indices = new unsigned int[3];
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+
+	Vertex vert = Vertex();
+	vert.Position = Vector3(-1.0f, -0.5f, 0.0f);
+	vertices[0] = vert;
+	vert.Position = Vector3(0.0f, 0.5f, 0.0f);
+	vertices[1] = vert;
+	vert.Position = Vector3(1.0f, -0.5f, 0.0f);
+	vertices[2] = vert;
+
+	m_pMesh = new Mesh(vertices, 3, indices, 3);
+
+	delete[] vertices;
+	delete[] indices;
 }
 
 void Simulation::Update(float a_fDeltaTime)
@@ -11,7 +32,7 @@ void Simulation::Update(float a_fDeltaTime)
 
 void Simulation::Draw(float a_fDeltaTime)
 {
-	float background[4] = { 0.4f, 0.6f, 0.75f, 0.0f };
+	float background[4] = { 0.1f, 0.8f, 0.5f, 0.0f };
 	// Clearing the screen.
 	Graphics::GetContext()->ClearRenderTargetView(
 		Graphics::GetBackBufferRTV().Get(),
@@ -19,6 +40,8 @@ void Simulation::Draw(float a_fDeltaTime)
 	Graphics::GetContext()->ClearDepthStencilView(
 		Graphics::GetDepthBufferDSV().Get(), 
 		D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	m_pMesh->Draw();
 
 	// Present at the end of the frame
 	bool vsync = Graphics::VsyncState();
@@ -39,4 +62,5 @@ void Simulation::OnResize()
 
 Simulation::~Simulation()
 {
+	SafeDelete(m_pMesh);
 }
--- SimulationEngine/Simulation.h
+++ SimulationEngine/Simulation.h
@@ -13,19 +13,43 @@
 class Simulation
 {
 private:
-
+	Mesh* m_pMesh = nullptr;
 
 public:
+	/// <summary>
+	/// Constructs the Simulation class.
+	/// </summary>
 	Simulation() = default;
+
+	/// <summary>
+	/// Frees up memory allocated by the Simulation class.
+	/// </summary>
 	~Simulation();
 
 	// There is no copy operator or constructor.
 	Simulation(const Simulation&) = delete;
 	Simulation& operator=(const Simulation&) = delete;
 
+	/// <summary>
+	/// Initializes variables used by the Simulation.
+	/// </summary>
 	void Init();
+
+	/// <summary>
+	/// Per frame logic updating method for the Simulation.
+	/// </summary>
+	/// <param name="a_fDeltaTime">The change in time between frames.</param>
 	void Update(float a_fDeltaTime);
+
+	/// <summary>
+	/// Per frame rendering method for the Simulation to its attached window context.
+	/// </summary>
+	/// <param name="a_fDeltaTime">The change in time between frames.</param>
 	void Draw(float a_fDeltaTime);
+
+	/// <summary>
+	/// Callback for when the screen is resized by the user.
+	/// </summary>
 	void OnResize();
 };
 
