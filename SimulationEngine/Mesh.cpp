19aa251f473aab77b97dc0f757369d054cdca8c0
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2025-09-04 14:34:41 -0400

    Added a enough logic to the Mesh class to make it functional for basic testing.

--- SimulationEngine/Mesh.cpp
+++ SimulationEngine/Mesh.cpp
@@ -1,7 +1,47 @@
 #include "Mesh.h"
+#include "Graphics.h"
 
 Mesh::Mesh(Vertex* a_pVertices, int a_dVertexCount, unsigned int* a_pIndices, int a_dIndexCount)
 {
+	// Saving the passed in values to the member fields.
+	m_pVertexBuffer = nullptr;
+	m_pIndexBuffer = nullptr;
+
+	m_dVertexCount = a_dVertexCount;
+	m_dIndexCount = a_dIndexCount;
+
+	// Calculating vertex tangents.
+	CalculateTangents(a_pVertices, a_dVertexCount, a_pIndices, a_dIndexCount);
+
+	// Setting up the vertex buffer setup struct object.
+	D3D11_BUFFER_DESC vbd = {};
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;				
+	vbd.ByteWidth = sizeof(Vertex) * a_dVertexCount;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;		
+	vbd.CPUAccessFlags = 0;							
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+
+	// Setting up the index buffer setup struct object.
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;					
+	ibd.ByteWidth = sizeof(unsigned int) * a_dIndexCount;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;			
+	ibd.CPUAccessFlags = 0;								
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+
+	// Creating the structs that actually hold the buffer data
+	D3D11_SUBRESOURCE_DATA initialVertexData = {};
+	D3D11_SUBRESOURCE_DATA initialIndexData = {};
+
+	// Setting the system memory to hold the buffer data.
+	initialVertexData.pSysMem = a_pVertices;
+	initialIndexData.pSysMem = a_pIndices;
+
+	// Creating the buffers.
+	Graphics::GetDevice()->CreateBuffer(&vbd, &initialVertexData, m_pVertexBuffer.GetAddressOf());
+	Graphics::GetDevice()->CreateBuffer(&ibd, &initialIndexData, m_pIndexBuffer.GetAddressOf());
 }
 
 Mesh::Mesh(const char* a_sFilepath)
@@ -18,17 +58,18 @@ Mesh::Mesh(const Mesh& a_pOther)
 
 Mesh& Mesh::operator=(const Mesh& a_pOther)
 {
-	// TODO: insert return statement here
+	Mesh mesh = Mesh(a_pOther);
+	return mesh;
 }
 
 BufferPtr Mesh::GetVertexBuffer(void)
 {
-	return BufferPtr();
+	return nullptr;
 }
 
 BufferPtr Mesh::GetIndexBuffer(void)
 {
-	return BufferPtr();
+	return nullptr;
 }
 
 int Mesh::GetIndexCount(void)
@@ -43,6 +84,21 @@ int Mesh::GetVertexCount(void)
 
 void Mesh::Draw(void)
 {
+	// Setting the stride to be the memory size of a Vertex
+	UINT stride = sizeof(Vertex);
+
+	// The offset starts at the first piece of data.
+	UINT offset = 0;
+
+	// Setting this Mesh's buffers as the next thing to draw.
+	Graphics::GetContext()->IASetVertexBuffers(0, 1, m_pVertexBuffer.GetAddressOf(), &stride, &offset);
+	Graphics::GetContext()->IASetIndexBuffer(m_pIndexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
+
+	// Starting up the render pipeline and drawing the currently set Index and Vertex buffers.
+	Graphics::GetContext()->DrawIndexed(
+		m_dIndexCount,		// The number of indices to use.
+		0,					// Offset from the first index to use.
+		0);					// Offset to add to each index.
 }
 
 void Mesh::CalculateTangents(Vertex* a_lVertices, int a_dVertexCount, unsigned int* a_lIndices, int a_dIndexCount)
