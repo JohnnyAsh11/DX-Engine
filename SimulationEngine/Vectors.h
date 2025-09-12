0676c32c52ff5a35dc6c9e50d11b6f3635361685
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2025-09-11 21:17:19 -0400

    Added a header file containing useful typedefs.

---
+++ SimulationEngine/Vectors.h
@@ -0,0 +1,8 @@
+#include <DirectXMath.h>
+
+// Renaming the standard vector types.
+typedef DirectX::XMFLOAT2 Vector2;
+typedef DirectX::XMFLOAT3 Vector3;
+typedef DirectX::XMFLOAT4 Vector4;
+
+typedef DirectX::XMVECTOR XMVector;
